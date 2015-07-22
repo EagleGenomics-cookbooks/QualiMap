7cd3b715219a665b81ecaa451f19d7726aa023d6
Bart Ailey <bart.ailey@eaglegenomics.com>
2015-07-22 12:02:37 +0100

    added .rubocop.yml

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,8 +3,7 @@ default['java']['jdk_version'] = 6
 default['java']['oracle']['accept_oracle_download_terms'] = true
 default['QualiMap']['version'] = 'v2.1.1'
 default['QualiMap']['filename'] = 'qualimap_' + default['QualiMap']['version'] + '.zip'
-default['QualiMap']['url'] = 'https://bitbucket.org/kokonech/qualimap/downloads/' + default['QualiMap']['filename']  
+default['QualiMap']['url'] = 'https://bitbucket.org/kokonech/qualimap/downloads/' + default['QualiMap']['filename']
 default['QualiMap']['install_dir'] = '/usr/local/'
 default['QualiMap']['dir'] = default['QualiMap']['install_dir'] + 'qualimap_' + default['QualiMap']['version']
-default['QualiMap']['bin_path'] = '/usr/local/bin'   
-
+default['QualiMap']['bin_path'] = '/usr/local/bin'
